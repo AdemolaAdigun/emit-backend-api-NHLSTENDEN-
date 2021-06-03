3bb9f56ac81ccc2ecc7c4d442df79be433006b5f
Tavitu97 <43606487+Tavitu97@users.noreply.github.com>
2021-06-03 15:07:06 +0300

    Admin

---
+++ src/controllers/user.js
@@ -0,0 +1,101 @@
+import {config} from 'dotenv';
+import Models from '../models';
+
+config();
+
+const {
+    Users,
+} = Models;
+
+export default {
+    getUserById: async (request, response) => {
+        const {id} = request.params;
+        const user = await Users.findByPk(id);
+
+        if(!user) {
+            response.status(404).json({
+                message: 'User not found.'
+            })
+        }
+
+        return response.status(200).json({
+            user,
+        });
+    },
+
+    getUsers: async (request, response) => {
+        const users = await Users.findAll();
+
+        if(!users) {
+            response.status(404).json({
+                message: 'Users not found.'
+            })
+        }
+
+        return response.status(200).json({
+            users,
+        });
+    },
+
+    createUser: async (request, response) => {
+        const {firstname, lastname, email, password, role, createdAt, updateAt} = request.body;
+        const {id} = request.params;
+
+        const user = await Users.create({
+            id,
+            firstname,
+            lastname,
+            email,
+            password,
+            role,
+            createdAt,
+            updateAt,
+        })
+        return response.status(200).json({
+            message: 'success',
+            user,
+        });
+    },
+
+    updateUser: async (request, response) => {
+        const {id} = request.params;
+        const users = await Users.findOne({
+            where: {
+                id,
+            }
+        });
+
+        await users.update(request.body, {
+            fields: Object.keys(request.body),
+        });
+        return response.status(200).json({
+            message: 'success',
+        });
+    },
+
+    deleteUser: async (request, response) => {
+        const {id} = request.params;
+
+        const user = await Users.findOne({
+            where: {
+                id,
+            }
+        });
+
+        if(!user) {
+            response.status(406).json({
+                message: 'Error on deleting.'
+            })
+        }
+
+        await Users.destroy({
+            where: {
+                id,
+            }
+        });
+
+        return response.status(200).json({
+            message: 'success',
+        });
+    },
+};
