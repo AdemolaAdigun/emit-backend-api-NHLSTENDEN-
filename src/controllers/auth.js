a5c0b125a1f9e4a2a2971b98ca69368c5cfaf0fa
AdemolaAdigun <Adigun673@gmail.com>
2021-06-28 10:29:24 +0200

    bug fix

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,53 +1,51 @@
-import { config } from 'dotenv';
-import { generateAuthToken } from '../helpers/auth';
+import {config} from 'dotenv';
+import {generateAuthToken} from '../helpers/auth';
 import Models from '../models';
 
 config();
 
 const {
 
-  User,
+    User,
 
 } = Models;
 
 
-
 export default {
+    loginUser: async (request, response) => {
+        const {
+            email,
+            password,
+        } = request.body;
 
+        const userEmail = await User.getExistingUser(email);
+        if (!userEmail) {
+            return response.status(401)
+                .json({
+                    status: 'error',
+                    message: 'Email or password is incorrect',
+                });
+        }
 
-loginUser: async (request, response) => {
-    const {
-      email,
-      password,
-    } = request.body;
+        const isPassword = userEmail.validatePassword(password);
+        if (!isPassword) {
+            return response.status(401)
+                .json({
+                    status: 'error',
+                    message: 'Email or password is incorrect',
+                });
+        }
 
-    const userEmail = await User.getExistingUser(email);
-    if (!userEmail) {
-      return response.status(401)
-        .json({
-          status: 'error',
-          message: 'Email or password is incorrect',
-        });
-    }
+        const user = userEmail.getSafeDataValues();
+        const token = generateAuthToken(user);
 
-    const isPassword = userEmail.validatePassword(password);
-    if (!isPassword) {
-      return response.status(401)
-        .json({
-          status: 'error',
-          message: 'Email or password is incorrect',
-        });
-    }
-
-    const user = userEmail.getSafeDataValues();
-    const token = generateAuthToken(user);
-
-    return response.status(200)
-      .json({
-        status: 'success',
-        data: {
-          user,
-          token,
-        },
-      });
-  },
+        return response.status(200)
+            .json({
+                status: 'success',
+                data: {
+                    user,
+                    token,
+                },
+            });
+    },
+}
