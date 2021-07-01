fcfd350ec25050d06f1673e37feddd27bbdab900
GherghelasAlexandru <47572770+GherghelasAlexandru@users.noreply.github.com>
2021-07-01 12:57:27 +0300

    added doc for projects and users

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -19,18 +19,45 @@ const {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /projects/:id
+ *  get:
+ *      description: Used for getting a project by id
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.get(
     '/:id',
     verifyToken,
     asyncWrapper(getProjectsById),
 );
 
+/**
+ * @swagger
+ * /projects/
+ *  get:
+ *      description: Used for getting all the projects
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.get(
     '/',
     verifyToken,
     asyncWrapper(getProjects)
 );
 
+/**
+ * @swagger
+ * /projects/create
+ *  post:
+ *      description: Used for creating a project
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.post(
     '/create',
     verifyToken,
@@ -38,18 +65,45 @@ router.post(
     asyncWrapper(createProject)
 );
 
+/**
+ * @swagger
+ * /projects/update/:id
+ *  put:
+ *      description: Used for updating a project
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.put(
     '/update/:id',
     verifyToken,
     asyncWrapper(updateProject)
 );
 
+/**
+ * @swagger
+ * /projects/delete/:id
+ *  delete:
+ *      description: Used for deleting a project
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.delete(
     '/delete/:id',
     verifyToken,
     asyncWrapper(deleteProject)
 );
 
+/**
+ * @swagger
+ * /projects/:projectId/add-attachment
+ *  post:
+ *      description: Used for adding an attachment
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.post(
     '/:projectId/add-attachment',
     Uploads.any(),
@@ -57,6 +111,15 @@ router.post(
     asyncWrapper(addAttachment)
 );
 
+/**
+ * @swagger
+ * /projects/remove-attachment/:attachmentId
+ *  delete:
+ *      description: Used for removing an attachment
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.delete(
     '/remove-attachment/:attachmentId',
     verifyToken,
--- src/routes/user.js
+++ src/routes/user.js
@@ -15,30 +15,75 @@ const {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /users/:id:
+ *  get:
+ *      description: Used for getting a user by id
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.get(
     '/:id',
     verifyToken,
     asyncWrapper(getUserById)
 );
 
+/**
+ * @swagger
+ * /users/:
+ *  get:
+ *      description: Used for getting all the users
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.get(
     '/',
     verifyToken,
     asyncWrapper(getUsers)
 );
 
+/**
+ * @swagger
+ * /users/create
+ *  post:
+ *      description: Used for creating a new user
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.post(
     '/create',
     verifyToken,
     asyncWrapper(createUser)
 );
 
+/**
+ * @swagger
+ * /users/update
+ *  put:
+ *      description: Used for updating an user
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.put(
     '/update',
     verifyToken,
     asyncWrapper(updateUser)
 );
 
+/**
+ * @swagger
+ * /users/delete/:id
+ *  delete:
+ *      description: Used for deleting an user
+ *      response:
+ *          '200':
+ *          description:
+ */
 router.delete(
     '/delete/:id',
     verifyToken,
